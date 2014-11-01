b709b6c17fb714ac59edd04b5c58a02ac07878ed
Arjan Topolovec <arjan.top@gmail.com>
2014-11-01 22:59:12 +0100

    Implemented writing of Little-Endian encoded varints.

--- src/varint.rs
+++ src/varint.rs
@@ -2,12 +2,16 @@ use std::io::{IoResult, IoError, InvalidInput};
 use std::iter::count;
 use std::num;
 
-pub trait Varint: FromPrimitive {
+pub trait Varint: FromPrimitive + ToPrimitive {
     fn varlen(&self) -> uint;
 }
 
 pub trait VarintReader {
     fn read_le_varint<V: Varint>(&mut self) -> IoResult<V>;
+}
+
+pub trait VarintWriter {
+    fn write_le_varint<V: Varint>(&mut self, x: V) -> IoResult<uint>;
 }
 
 fn varint_length(mut x: u64) -> uint {
@@ -67,13 +71,14 @@ impl<R> VarintReader for R where R: Reader {
     }
 }
 
+
 fn read_le_varint<R: Reader>(reader: &mut R) -> IoResult<u64> {
     let mut x = 0u64;
     let mut shift = 0u;
     for i in count(0u, 1) {
         let b = try!(reader.read_byte());
         if b < 0b1000_0000 {
-            if (i == 9 && b > 1) || i > 9 {
+            if (i == 9 && b > 1) || i >= 10 {
                 return Err(OWERFLOW_ERROR.clone())
             }
             return Ok(x | b as u64 << shift)
@@ -84,12 +89,29 @@ fn read_le_varint<R: Reader>(reader: &mut R) -> IoResult<u64> {
     unreachable!();
 }
 
+impl<W> VarintWriter for W where W: Writer {
+    fn write_le_varint<V: Varint>(&mut self, x: V) -> IoResult<uint> {
+        write_le_varint(self, x.to_u64().unwrap())
+    }
+}
+
+fn write_le_varint<W: Writer>(writer: &mut W, mut x: u64) -> IoResult<uint> {
+    let mut i = 0;
+    while x >= 0b1000_0000 {
+        try!(writer.write_u8(x as u8 | 0b1000_0000));
+        x >>= 7;
+        i += 1;
+    }
+    try!(writer.write_u8(x as u8));
+    Ok(i + 1)
+}
+
 #[cfg(test)]
 mod test {
-    use std::io::{BufReader, IoResult};
+    use std::io::{BufReader, BufWriter, IoResult, OtherIoError};
     use std::fmt::Show;
 
-    use super::{VarintReader, Varint, OWERFLOW_ERROR};
+    use super::{VarintReader, VarintWriter, Varint, OWERFLOW_ERROR};
 
     fn test_read_le_varint<V: Varint + PartialEq + Show>(buf: &[u8], expected: IoResult<V>) {
         let mut r = BufReader::new(buf);
@@ -103,12 +125,43 @@ mod test {
         test_read_le_varint([0x80, 0x01], Ok(0x80u32));
         test_read_le_varint([0xAC, 0x02], Ok(300u32));
         test_read_le_varint([0x80, 0x01], Ok(0x80u8));
-        test_read_le_varint::<u64>([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01], Ok(0xFFFFFFFFFFFFFFFFu64));
+        test_read_le_varint([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01], Ok(0xFFFFFFFFFFFFFFFFu64));
     }
 
     #[test]
     fn read_le_varint_owerflow() {
         test_read_le_varint::<u8>([0xAC, 0x02], Err(OWERFLOW_ERROR.clone()));
         test_read_le_varint::<u64>([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F], Err(OWERFLOW_ERROR.clone()));
+    }
+
+    fn test_write_le_varint<V: Varint + PartialEq + Show>(x: V, result: &[u8]) {
+        let mut buf = [0, ..10];
+        let n = {
+            let mut r = BufWriter::new(&mut buf);
+            let r = r.write_le_varint(x);
+            assert_eq!(Ok(result.len()), r);
+            r.unwrap()
+        };
+        assert_eq!(buf[.. n], result);
+    }
+
+    #[test]
+    fn write_le_varint() {
+        test_write_le_varint(0x00u32, [0x00]);
+        test_write_le_varint(0x7Fu32, [0x7F]);
+        test_write_le_varint(0x80u32, [0x80, 0x01]);
+        test_write_le_varint(300u32, [0xAC, 0x02]);
+        test_write_le_varint(0x80u8, [0x80, 0x01]);
+        test_write_le_varint(0xFFFFFFFFFFFFFFFFu64, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x01]);
+    }
+
+    #[test]
+    fn write_le_varint_erro_is_reported() {
+        let mut buf = [0, ..1];
+        let mut r = BufWriter::new(&mut buf);
+        match r.write_le_varint(0x80u32) {
+            Err(ref err) if err.kind == OtherIoError => {},
+            res => fail!(format!("{}", res)),
+        }
     }
 }
